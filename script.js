963dcd17264c0deb1bde8946e78da42e161ebc89
Marlon Veiga <marlon_veiga@hotmail.com>
2021-06-12 00:35:42 -0300

    Define snake directions and avoid wall collisions.

--- script.js
+++ script.js
@@ -20,17 +20,32 @@ function createSnake() {
     }
 }
 
+document.addEventListener("keydown", update);
+
+function update(event) {
+    if (event.keyCode == 37 && direction != "right") direction = "left";
+    if (event.keyCode == 38 && direction != "down") direction = "up";
+    if (event.keyCode == 39 && direction != "left") direction = "right";
+    if (event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function startGame() {
+
+    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
     createBG();
     createSnake();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
-    if (direction === "right") snakeX += box;
-    if (direction === "left") snakeX -= box;
-    if (direction === "up") snakeY -= box;
-    if (direction === "down") snakeX += box;
+    if (direction == "right") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
 
     snake.pop();
 
@@ -42,4 +57,4 @@ function startGame() {
     snake.unshift(newHead)
 }
 
-let jogo = setInterval(startGame, 100);
+let jogo = setInterval(startGame, 200);
